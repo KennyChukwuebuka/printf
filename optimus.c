40662357f2192e3143be3da0dcd5d57460a6de99
Yandah1 <nonku003@gmail.com>
2023-07-25 20:16:58 +0200

    updated

---
+++ optimus.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * optimus - Searches for the appropriate printing
+ * function based on the input format string.
+ * @format: The format string to search for.
+ *
+ * Return: A pointer to the appropriate printing function.
+ * If no function is found, returns NULL.
+ */
+int (*optimus(const char *format))(char *format, va_list)
+{
+	int i;
+
+	strtype megatron[] = {
+		{"%c", _printc},
+		{"%s", _printstr},
+		{"%%", printpercentsign},
+		{"%d", printint},
+		{"%i", printint},
+		{"%o", _printocta},
+		{"%x", _printhex},
+		{"%X", _printHEX},
+		{"%p", _printpointer},
+		{NULL, NULL}
+	};
+	if (format[1] == ' ' || format[1] == '\0')
+		return (NULL);
+	/*if (format == NULL || format[0] == '\0' || format[1] == '\0')*/
+	/* return (NULL);*/
+
+	for (i = 0; megatron[i].v; i++)
+	{
+		if (format[1] == megatron[i].v[1])
+			return (megatron[i].w);
+	}
+	return (NULL);
+}
